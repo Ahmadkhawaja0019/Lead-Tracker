c14d2d5a3ccb2d6bbe005978f8266e3d2bd2b11d
Hafiz Muhammad Ahmad <i230714@isb.nu.edu.pk>
2025-07-01 00:20:53 +0500

    save tab function added in code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let myLeads = []
-
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -26,19 +25,17 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
-})
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
 })
 
 tabBtn.addEventListener("click", function(){    
